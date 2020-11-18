598df7faa060d1ca0d24c47cc7ea41798f79f94f
Yousef <namiyousef@hotmail.com>
2020-11-18 10:21:38 +0100

    added setup file to test if the environment has loaded correctly (outdated)

---
+++ Setup_test.py
@@ -0,0 +1,12 @@
+"""
+Author: Yousef Nami
+Name: 07.10.20
+Changes log:
+    v.0.0.0.    Created file, tested that all items installed in correct virtualenv
+"""
+import numpy
+import pandas
+import matplotlib
+import tensorflow
+import keras
+
